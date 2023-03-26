f720ff5ccaa3764a4344fde3f7748601ffe16e1a
Martin Bachmann <martinpezbach@gmail.com>
2023-03-26 15:53:52 -0300

    feat: implement search_by_word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -20,4 +20,26 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    resposta = []
+
+    for index in range(0, len(instance)):
+        arquivo = instance.search(index)
+        dados_do_arquivo = {
+            "palavra": word,
+            "arquivo": arquivo["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for index_2 in range(0, arquivo["qtd_linhas"]):
+            if word.lower() in arquivo["linhas_do_arquivo"][index_2].lower():
+                dados_do_arquivo["ocorrencias"].append(
+                    {
+                        "linha": index_2 + 1,
+                        "conteudo": arquivo["linhas_do_arquivo"][index_2],
+                    }
+                )
+
+        if len(dados_do_arquivo["ocorrencias"]) > 0:
+            resposta.append(dados_do_arquivo)
+
+    return resposta
